4e5b8b868153ab15d580ed75e7c331eafc870547
yewon9609 <dodnjs1241@naver.com>
2022-03-24 20:50:13 +0900

    day01

---
+++ day01/src/day01/PrintTest.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		
+		//주석
+		//1.소스코드에 설명글을 달 때
+		//2.지금 당장 사용하지 않는 코드를 번역하고 싶지 않을 때
+		
+		//주석의 종류(한 줄 주석)
+		
+		/*
+		  1.한줄 주석: Ctrl+/
+		  2.범위 주석:범위선택 후 , Ctrl + Shift+/, 해제시:Ctrl+Shift+\
+		 */
+		
+//		이름을 출력하는 부분 
+		System.out.print("서예원");
+		
+//		나이를 출력하는 부분
+		System.out.print("20살");
+		
+		
+		
+	}	
+}
